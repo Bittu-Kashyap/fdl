5cc06191bc4cbf2d1dbd32fc49bf695ba82de67f
Bittu-Kashyap <bittukashyap22@gmail.com>
2026-04-10 17:39:56 +0530

    Delete test_time.php

--- test_time.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-$stmt = $conn->query("SELECT NOW()");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-$stmt2 = $conn->query("SELECT CURRENT_TIMESTAMP");
-print_r($stmt2->fetchAll(PDO::FETCH_ASSOC));
-echo "PHP Time: " . date('Y-m-d H:i:s') . "\n";
-?>
